39ce8fb662a245ecdcfba97ec1026565f9dc3f9b
Matt Chadburn <matt.chadburn@guardian.co.uk>
2012-06-26 19:34:19 +0100

    correct conditional on printed output

--- filter.rb
+++ filter.rb
@@ -10,7 +10,7 @@ STDIN.read.split("\n").each do |entry|
     
     log = Log.new().parse entry
 
-    if phone = wurfl.lookup( entry.ua)
+    if phone = wurfl.lookup( log.ua )
         puts [log.ip, log.status, log.method, log.ua, log.resource, phone[:device_os_version], phone[:device_os]].join("\t")
     else
         puts ['unknown', nil, nil, log.ua].join("\t")
